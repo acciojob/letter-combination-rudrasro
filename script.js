02475ef1dbe27b2ce58c462039673f72d5e1e510
Supriyo Rudra <117836404+rudrasro@users.noreply.github.com>
2023-01-12 15:38:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
 function letterCombinations(input_digit) {
   //Complete the function
+	if (!input_digit) return []; // return empty array if input is empty or null
+  let map = {
+      '2': 'abc',
+      '3': 'def',
+      '4': 'ghi',
+      '5': 'jkl',
+      '6': 'mno',
+      '7': 'pqrs',
+      '8': 'tuv',
+      '9': 'wxyz'
+  };
+  let result = ['']; // start with an array containing an empty string
+  for (let i = 0; i < input_digit.length; i++) {
+      let temp = [];
+      for (let j = 0; j < result.length; j++) {
+          for (let k = 0; k < map[input_digit[i]].length; k++) {
+              temp.push(result[j] + map[input_digit[i]][k]);
+          }
+      }
+      result = temp;
+  }
+  return result;
 }
 
 module.exports = letterCombinations;
